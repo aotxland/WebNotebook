b3d8979e414441b70cb1c700263ee687750721ff
AoTxLand <aotxland@gmail.com>
2020-05-08 21:14:28 +0800

    fix bugs

---
+++ themes/hugo-theme-terminal/Makefile
@@ -0,0 +1,11 @@
+build:
+	npm run build
+
+install:
+	npm install
+
+dev:
+	npm run dev
+
+up:
+	npm up --verbose
---
+++ themes/hugo-theme-terminal/source/js/main.js
@@ -0,0 +1,7 @@
+// Add your script here
+
+import baguetteBox from 'baguettebox.js'
+
+window.addEventListener('load', function() {
+  baguetteBox.run('.post-content .post-img-lb')
+})
